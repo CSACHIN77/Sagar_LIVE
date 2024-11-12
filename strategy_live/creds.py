c7cc24b3ede8b653ca169b819a5aa41d963b2097
Deepak Kumar <hftdeepak@gmail.com>
2024-11-12 21:48:51 +0530

    add strategy live

---
+++ strategy_live/creds.py
@@ -0,0 +1,7 @@
+creds = {
+    'market_key': "1f440830af1d82a7a09251",
+    'market_secret': "Dllf432@Co",
+    'interactive_key': "e409ac8bd3f08ffc796583",
+    'interactive_secret': "Htuh007@ub",
+    'port': "https://ttblaze.iifl.com"
+}
